ab1cc12c138e897369246d496810677aec120680
Simon Chenard <chenard.simon@gmail.com>
2016-11-04 11:07:21 +0100

    more ugliness, wrap around the exception to get more info

--- vcrwrapper/wrapper.py
+++ vcrwrapper/wrapper.py
@@ -3,11 +3,11 @@ from functools import wraps
 from . import vcrutils
 from .utils import dual_decorator
 from .exceptions import InvalidModeException
- 
+
 VCR_CASSETTE_PATH = os.environ.get('VCR_CASSETTE_PATH')
 MAKE_EXTERNAL_REQUESTS = os.environ.get('VCR_MAKE_EXTERNAL_REQUESTS') == 'TRUE'
 RECORD_MODE = os.environ.get('VCR_RECORD_MODE', 'none')
- 
+
 @dual_decorator  # convert a paramaterized decorator for no-arg use (https://gist.github.com/simon-weber/9956622).
 def external_call(*args, **kwargs):
     """Enable vcrpy to store/mock http requests.
@@ -40,46 +40,50 @@ def external_call(*args, **kwargs):
 
     Class method tests are also supported.
     """
- 
+
     use_namespaces = kwargs.pop('use_namespaces', False)
- 
+
     vcr_args = args
     vcr_kwargs = kwargs
 
     if RECORD_MODE not in ['once', 'new_episode', 'none', 'all']:
         raise InvalidModeException('Invalid record_mode {} please check your VCR_RECORD_MODE environment variable', RECORD_MODE)
- 
+
     default_vcr_kwargs = {
         'cassette_library_dir': VCR_CASSETTE_PATH,
         'record_mode': RECORD_MODE,
         'match_on': ('method', 'scheme', 'host', 'port', 'path', 'json_query'),
         'filter_headers': ['authorization']
     }
- 
+
     default_vcr_kwargs.update(vcr_kwargs)
- 
+
     match_on = default_vcr_kwargs.pop('match_on')
- 
+
     def decorator(f, vcr_args=vcr_args, vcr_kwargs=default_vcr_kwargs,
                   match_on=match_on, use_namespaces=use_namespaces):
- 
+
         # this is used as a nose attribute:
         #  http://nose.readthedocs.org/en/latest/plugins/attrib.html
         f.external_api = True
- 
+
         @wraps(f)
         def wrapper(*args, **kwargs):
             my_vcr = vcrutils.get_vcr(*vcr_args, **vcr_kwargs)
             cassette_filename = vcrutils.get_filename_from_method(f, args[0])
- 
+
             if use_namespaces:
                 kwargs['vcr_namespace'] = vcrutils.get_namespace_cm(
                     my_vcr, cassette_filename, MAKE_EXTERNAL_REQUESTS)
- 
+
             if MAKE_EXTERNAL_REQUESTS:
                 return f(*args, **kwargs)
             else:
-                with my_vcr.use_cassette(cassette_filename, match_on=match_on):
-                    return f(*args, **kwargs)
+                try:
+                    with my_vcr.use_cassette(cassette_filename, match_on=match_on):
+                        return f(*args, **kwargs)
+                except Exception as e:
+                    msg = "Inside VCRwrapper, {}\r\n{}".format(" / ".join(match_on), e)
+                    raise Exception(msg)
         return wrapper
     return decorator
